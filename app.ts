25fb3b2c400ae762256fec90d3adc931454e2864
Natalie-Cardot <nataliecardot@gmail.com>
2020-07-29 18:18:47 -0700

    Use type inference and type casting

--- app.ts
+++ app.ts
@@ -1,6 +1,20 @@
+export {};
+
+// Using type casting (otherwise cannot use num1Element.value; not every HTML element has a value property, so have to inform TS that what we get access to here is an input)
+const num1Element = document.getElementById('num1') as HTMLInputElement;
+const num2Element = document.getElementById('num2') as HTMLInputElement;
+// type is inferred by TypeScript (it knows it's a button since query select by button tag, so when entering buttonElement, addEventListener comes up as a method. It knows that when selecting button with querySelector, we get an HTMLButtonElement, the inferred type, one of the many built-in types supported by TS)
+const buttonElement = document.querySelector('button');
+
 // To compile to JavaScript, run tsc app.ts (having globally installed TypeScript on computer with npm i -g typescript)
 function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-console.log(add(1, 6));
+buttonElement.addEventListener('click', () => {
+  const num1 = num1Element.value;
+  const num2 = num2Element.value;
+  // value property always has a return type of string, so using unary plus to convert to number
+  const result = add(+num1, +num2);
+  console.log(result);
+});
