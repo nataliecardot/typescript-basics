b273c0e811ca0e3beb86a789052d6304129e8ac1
Natalie-Cardot <nataliecardot@gmail.com>
2020-07-29 17:45:51 -0700

    Assign types

--- app.ts
+++ app.ts
@@ -1,8 +1,6 @@
 // To compile to JavaScript, run tsc app.ts (having globally installed TypeScript on computer with npm i -g typescript)
-function add(num1, num2) {
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
 console.log(add(1, 6));
-
-console.log(add('1', '6'));
